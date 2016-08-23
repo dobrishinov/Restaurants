98a3b8c926755557c70b427ea4cf740353e5bad3
Georgi Dobrishinov <dobrishinov@gmail.com>
2016-08-23 10:46:16 +0300

    Fix Auth Service

--- DataAccess/Service/Auth.cs
+++ DataAccess/Service/Auth.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccess.Service
+﻿namespace DataAccess.Service
 {
-    class Auth
+    using System.Linq;
+    using Repository;
+    using Entity;
+
+    public class Auth
     {
+        public UserEntity LoggedUser { get; private set; }
+
+        public void Authenticate(string username, string password)
+        {
+            UsersRepository usersRepository = new UsersRepository();
+            LoggedUser = usersRepository.GetAll(u => u.Username == username && u.Password == password).FirstOrDefault();
+
+            if (LoggedUser == null)
+            {
+                
+                LoggedUser = usersRepository.GetAll(u => u.Username == username && u.Password == password).FirstOrDefault();
+
+            }
+        }
     }
 }
